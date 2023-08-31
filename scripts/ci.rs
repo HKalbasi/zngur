8615faa60e42163ded6ffe0be38883eae90cf515
hkalbasi <hamidrezakalbasi@protonmail.com>
2023-09-01 00:58:16 +0330

    Fix ci

--- scripts/ci.rs
+++ scripts/ci.rs
@@ -12,7 +12,15 @@
 use anyhow::{Context, Result};
 use xshell::{cmd, Shell};
 
-fn check_examples(sh: Shell) -> Result<()> {
+fn check_crate(sh: &Shell) -> Result<()> {
+    cmd!(sh, "cargo check").run()?;
+    cmd!(sh, "cargo fmt --check")
+        .run()
+        .with_context(|| "Crate is not formatted. Run `cargo fmt`")?;
+    Ok(())
+}
+
+fn check_examples(sh: &Shell) -> Result<()> {
     const CARGO_PROJECTS: &[&str] = &["cxx_demo"];
     sh.change_dir("examples");
     let examples = cmd!(sh, "ls").read()?;
@@ -39,9 +47,15 @@ fn check_examples(sh: Shell) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    let sh = Shell::new()?;
+    let sh = &Shell::new()?;
     sh.set_var("RUSTFLAGS", "-D warnings");
-    cmd!(sh, "cargo check").run()?;
+    for dir in cmd!(sh, "ls").read()?.lines() {
+        if sh.path_exists(format!("{dir}/Cargo.toml")) {
+            sh.change_dir(dir);
+            check_crate(sh).with_context(|| format!("Checking crate {dir} failed"))?;
+            sh.change_dir("..")
+        }
+    }
     check_examples(sh).with_context(|| "Checking examples failed")?;
     Ok(())
 }
