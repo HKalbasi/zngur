38a9f92e78b530eef749acd3fbb9631b6e4ad4c0
hkalbasi <hamidrezakalbasi@protonmail.com>
2023-09-05 00:38:28 +0330

    Remove install osmium xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,18 +1,15 @@
 use clap::Parser;
 
 mod ci;
-mod install_osmium;
 
 #[derive(Parser)]
 enum Command {
     CI,
-    InstallOsmium,
 }
 
 fn main() -> anyhow::Result<()> {
     let cmd = Command::parse();
     match cmd {
         Command::CI => ci::main(),
-        Command::InstallOsmium => install_osmium::main(),
     }
 }
