373ebeae1f2ab6bfb83f2c3a0951c731e05e15a4
Nick DeMarco <demarco@adobe.com>
2025-08-07 13:01:18 -0400

    Support import directives in parser

--- zngur-parser/src/tests.rs
+++ zngur-parser/src/tests.rs
@@ -3,18 +3,32 @@ use std::panic::catch_unwind;
 use expect_test::{Expect, expect};
 use zngur_def::{RustPathAndGenerics, RustType};
 
-use crate::ParsedZngFile;
+use crate::{ImportResolver, ParsedZngFile};
 
 fn check_success(zng: &str) {
-    let _ = ParsedZngFile::parse("main.zng", zng);
+    let _ = ParsedZngFile::parse_str(zng);
 }
 
 pub struct ErrorText(pub String);
 
 fn check_fail(zng: &str, error: Expect) {
     let r = catch_unwind(|| {
-        let _ = ParsedZngFile::parse("main.zng", zng);
+        let _ = ParsedZngFile::parse_str(zng);
     });
+    match r {
+        Ok(_) => panic!("Parsing succeeded but we expected fail"),
+        Err(e) => match e.downcast::<ErrorText>() {
+            Ok(t) => error.assert_eq(&t.0),
+            Err(e) => std::panic::resume_unwind(e),
+        },
+    }
+}
+
+fn check_import_fail(zng: &str, error: Expect, resolver: &MockFilesystem) {
+    let r = catch_unwind(|| {
+        let _ = ParsedZngFile::parse_str_with_resolver(zng, resolver);
+    });
+
     match r {
         Ok(_) => panic!("Parsing succeeded but we expected fail"),
         Err(e) => match e.downcast::<ErrorText>() {
@@ -35,7 +49,7 @@ type () {
     "#,
         expect![[r#"
             Error: Unit type is declared implicitly. Remove this entirely.
-               ╭─[main.zng:2:6]
+               ╭─[test.zng:2:6]
                │
              2 │ type () {
                │      ─┬  
@@ -67,7 +81,7 @@ type () {
     "#,
         expect![[r#"
             Error: found 'welcome_traits' expected '#', 'wellknown_traits', 'constructor', 'field', 'fn', or '}'
-               ╭─[main.zng:4:5]
+               ╭─[test.zng:4:5]
                │
              4 │     welcome_traits(Copy);
                │     ───────┬──────  
@@ -88,7 +102,7 @@ type ::std::string::String {
     "#,
         expect![[r#"
             Error: Duplicate layout policy found
-               ╭─[main.zng:4:5]
+               ╭─[test.zng:4:5]
                │
              4 │     #heap_allocated;
                │     ───────┬───────  
@@ -110,7 +124,7 @@ type crate::Way {
     "#,
         expect![[r#"
             Error: Duplicate layout policy found
-               ╭─[main.zng:3:5]
+               ╭─[test.zng:3:5]
                │
              3 │     #layout(size = 1, align = 2);
                │     ──────────────┬─────────────  
@@ -129,8 +143,7 @@ type crate::Way {
 
 #[test]
 fn alias_expands_correctly() {
-    let parsed = ParsedZngFile::parse(
-        "main.zng",
+    let parsed = ParsedZngFile::parse_str(
         r#"
 use ::std::string::String as MyString;
 type MyString {
@@ -147,8 +160,7 @@ type MyString {
 
 #[test]
 fn alias_expands_nearest_scope_first() {
-    let parsed = ParsedZngFile::parse(
-        "main.zng",
+    let parsed = ParsedZngFile::parse_str(
         r#"
 use ::std::string::String as MyString;
 mod crate {
@@ -164,4 +176,224 @@ mod crate {
         panic!("no match?");
     };
     assert_eq!(p.as_slice(), ["crate", "MyLocalString"]);
+}
+
+struct MockFilesystem {
+    files: std::collections::HashMap<std::path::PathBuf, String>,
+}
+
+impl MockFilesystem {
+    fn new(
+        files: impl IntoIterator<Item = (impl Into<std::path::PathBuf>, impl Into<String>)>,
+    ) -> Self {
+        Self {
+            files: files
+                .into_iter()
+                .map(|(k, v)| (k.into(), v.into()))
+                .collect(),
+        }
+    }
+}
+
+impl ImportResolver for MockFilesystem {
+    fn resolve_import(
+        &self,
+        cwd: &std::path::Path,
+        relpath: &std::path::Path,
+    ) -> Result<String, String> {
+        let path = cwd.join(relpath);
+        self.files
+            .get(&path)
+            .cloned()
+            .ok_or_else(|| format!("File not found: {}", path.display()))
+    }
+}
+
+#[test]
+fn import_parser_test() {
+    let resolver = MockFilesystem::new(vec![(
+        "./relative/path.zng",
+        "type Imported { #layout(size = 1, align = 1); }",
+    )]);
+
+    let parsed = ParsedZngFile::parse_str_with_resolver(
+        r#"
+import "./relative/path.zng";
+type Example {
+    #layout(size = 1, align = 1);
+}
+    "#,
+        &resolver,
+    );
+    assert_eq!(parsed.types.len(), 2);
+}
+
+#[test]
+fn module_import_prohibited() {
+    let resolver = MockFilesystem::new(vec![] as Vec<(&str, &str)>);
+
+    check_import_fail(
+        r#"
+    import "foo/bar.zng";
+    "#,
+        expect![[r#"
+            Error: Module import is not supported. Use a relative path instead.
+               ╭─[test.zng:2:5]
+               │
+             2 │     import "foo/bar.zng";
+               │     ──────────┬──────────  
+               │               ╰──────────── Module import is not supported. Use a relative path instead.
+            ───╯
+        "#]],
+        &resolver,
+    );
+}
+
+#[test]
+fn import_has_conflict() {
+    // Test that an import which introduces a conflict produces a reasonable error message.
+    let resolver = MockFilesystem::new(vec![(
+        "./a.zng",
+        r#"
+      type A {
+        #layout(size = 1, align = 1);
+      }
+    "#,
+    )]);
+
+    check_import_fail(
+        r#"
+    import "./a.zng";
+    type A {
+      #layout(size = 2, align = 2);
+    }
+"#,
+        expect![[r#"
+            Error: Duplicate layout policy found
+               ╭─[a.zng:2:12]
+               │
+             2 │       type A {
+               │            ┬  
+               │            ╰── Duplicate layout policy found
+            ───╯
+        "#]],
+        &resolver,
+    );
+}
+
+#[test]
+fn import_not_found() {
+    let resolver = MockFilesystem::new(vec![] as Vec<(&str, &str)>);
+    check_import_fail(
+        r#"
+    import "./a.zng";
+    "#,
+        expect![[r#"
+            Error: Import path not found: ./a.zng
+        "#]],
+        &resolver,
+    );
+}
+
+#[test]
+fn import_has_mismatched_method_signature() {
+    let resolver = MockFilesystem::new(vec![(
+        "./a.zng",
+        "type A { #layout(size = 1, align = 1); fn foo(i32) -> i32; }",
+    )]);
+
+    check_import_fail(
+        r#"
+  import "./a.zng";
+  type A {
+    #layout(size = 1, align = 1);
+    fn foo(i64) -> i64;
+  }
+  "#,
+        expect![[r#"
+            Error: Method mismatch
+               ╭─[a.zng:1:6]
+               │
+             1 │ type A { #layout(size = 1, align = 1); fn foo(i32) -> i32; }
+               │      ┬  
+               │      ╰── Method mismatch
+            ───╯
+        "#]],
+        &resolver,
+    );
+}
+
+#[test]
+fn import_has_mismatched_field() {
+    let resolver = MockFilesystem::new(vec![(
+        "./a.zng",
+        "type A {
+        #layout(size = 1, align = 1);
+        field x (offset = 0, type = i32);
+    }",
+    )]);
+
+    check_import_fail(
+        r#"
+  import "./a.zng";
+  type A {
+    #layout(size = 1, align = 1);
+    field x (offset = 0, type = i64);
+  }
+  "#,
+        expect![[r#"
+            Error: Field mismatch
+               ╭─[a.zng:1:6]
+               │
+             1 │ type A {
+               │      ┬  
+               │      ╰── Field mismatch
+            ───╯
+        "#]],
+        &resolver,
+    );
+}
+
+#[test]
+fn convert_panic_to_exception_in_imported_file_fails() {
+    let resolver = MockFilesystem::new(vec![(
+        "./imported.zng",
+        r#"
+        #convert_panic_to_exception
+        type A {
+            #layout(size = 1, align = 1);
+        }
+        "#,
+    )]);
+
+    check_import_fail(
+        r#"
+import "./imported.zng";
+type B {
+    #layout(size = 1, align = 1);
+}
+        "#,
+        expect![[r#"
+            Error: Using `#convert_panic_to_exception` in imported zngur files is not supported. This directive can only be used in the main zngur file.
+               ╭─[imported.zng:2:10]
+               │
+             2 │         #convert_panic_to_exception
+               │          ─────────────┬────────────  
+               │                       ╰────────────── Using `#convert_panic_to_exception` in imported zngur files is not supported. This directive can only be used in the main zngur file.
+            ───╯
+        "#]],
+        &resolver,
+    );
+}
+
+#[test]
+fn convert_panic_to_exception_in_main_file_succeeds() {
+    check_success(
+        r#"
+#convert_panic_to_exception
+type A {
+    #layout(size = 1, align = 1);
+}
+        "#,
+    );
 }
