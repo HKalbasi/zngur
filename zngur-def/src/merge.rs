ff5153b130c24e6dd584132bc5af8d7f40b3ec97
Colin Schultz <colin.r.schultz@gmail.com>
2026-01-09 09:02:58 +0100

    fix typo

--- zngur-def/src/merge.rs
+++ zngur-def/src/merge.rs
@@ -135,7 +135,7 @@ impl Merge for ZngurType {
 
         if let (Some(layout1), Some(layout2)) = &(self.layout, into.layout) && layout1 != layout2 {
             return Err(MergeFailure::Conflict(
-                "Conflictng layout policy found".to_string(),
+                "Conflicting layout policy found".to_string(),
             ));
         } else {
             into.layout = into.layout.or(self.layout);
