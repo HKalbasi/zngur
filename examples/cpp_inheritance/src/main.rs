a0a1fe5d9cbd2174ed1b0fa2d1d710c50e3fd9f0
Adam Snaider <asnaider@google.com>
2026-04-11 12:48:08 -0400

    Add a cpp_inheritance example

---
+++ examples/cpp_inheritance/src/main.rs
@@ -0,0 +1,40 @@
+#![allow(non_camel_case_types)]
+
+#[rustfmt::skip]
+mod generated;
+
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll, Waker};
+
+use cpp_inherit::CppInherit;
+use znglib::ZngCppStackObject as _;
+
+use crate::generated::cpp::{CppTask, Dispatcher};
+
+pub struct RustTask<'a>(pub Pin<&'a mut dyn Future<Output = ()>>);
+
+impl<'a> RustTask<'a> {
+    pub fn poll(&mut self) -> Poll<()> {
+        let waker = unsafe { Waker::from_raw(dummy_raw_waker()) };
+        let mut cx = Context::from_waker(&waker);
+        self.0.as_mut().poll(&mut cx)
+    }
+}
+
+unsafe fn dummy_raw_waker() -> std::task::RawWaker {
+    static DUMMY_WAKER_VTABLE: std::task::RawWakerVTable =
+        std::task::RawWakerVTable::new(|_| unsafe { dummy_raw_waker() }, |_| (), |_| (), |_| ());
+    std::task::RawWaker::new(std::ptr::null(), &DUMMY_WAKER_VTABLE)
+}
+
+fn main() {
+    let mut fut = async {
+        println!("Hello from Rust async!");
+    };
+
+    let fut = RustTask(unsafe { Pin::new_unchecked(&mut fut) });
+    let mut task: CppInherit<CppTask, RustTask> = CppInherit::new(fut, CppTask::new());
+    let dispatcher = Dispatcher::new();
+    dispatcher.run_task(&mut task.inner);
+}
